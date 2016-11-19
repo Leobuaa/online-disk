661178fbf0580e0762177f949f1c79b76adf4194
Leo Peng <pengzimaobuaa@163.com>
2016-11-19 12:14:55 +0800

    改变项目目录结构

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginRegisterBlock from './LoginRegisterBlock.js';
+import LoginRegisterBlock from './components/LoginRegisterBlock.js';
 
 class App extends Component {
   render() {
