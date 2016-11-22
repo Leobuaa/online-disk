c334f115c74acfd5b211d733c859e8e66dc00e2e
Leo Peng <pengzimaobuaa@163.com>
2016-11-22 17:17:06 +0800

    update

--- src/components/LoginRegisterTab.js
+++ src/components/LoginRegisterTab.js
@@ -9,19 +9,17 @@ class LoginRegisterTab extends Component {
 
   handleRegisterClick(event) {
     this.props.onRegisterTabClick();
-    event.preventDefault();
   }
 
   handleLoginClick(event) {
     this.props.onLoginTabClick();
-    event.preventDefault();
   }
 
   render() {
     return (
       <div className="navs-slider" data-active-index={this.props.activeTab === 'register' ? '0' : '1'}>
-          <a href="#" className={this.props.activeTab === 'register' ? 'active' : ''} onClick={this.handleRegisterClick}>注册</a>
-          <a href="#" className={this.props.activeTab === 'login' ? 'active' : ''} onClick={this.handleLoginClick}>登录</a>
+          <a href="#register" className={this.props.activeTab === 'register' ? 'active' : ''} onClick={this.handleRegisterClick}>注册</a>
+          <a href="#login" className={this.props.activeTab === 'login' ? 'active' : ''} onClick={this.handleLoginClick}>登录</a>
           <span className="navs-slider-bar"></span>
       </div>
     );
