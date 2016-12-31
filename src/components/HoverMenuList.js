fe62ae589b8e87e12c3105ff2cf23b0c87aabfa1
Leo Peng <pengzimaobuaa@163.com>
2016-12-31 14:22:26 +0800

    finish completeDelete.

--- src/components/HoverMenuList.js
+++ src/components/HoverMenuList.js
@@ -107,6 +107,49 @@ class HoverMenuList extends Component {
       })
   }
 
+  handleCompleteDeleteButtonClick(event) {
+    event.target.blur();
+    const activeLists = this.props.bodyContent.activeLists;
+    const listCheckedIds = [this.props._id];
+
+    const confirmClick = () => {
+      const fetchLink = Helper.fetchLinkHeader + 'completeDelete';
+      const params = {
+        ids: listCheckedIds,
+      };
+      fetch(fetchLink, {
+        method: 'POST',
+        body: JSON.stringify(params),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then((response) => response.json())
+        .then((json) => {
+          if (json.success === '1' || json.success === 1) {
+            this.props.onUpdateActiveLists(activeLists.filter((obj) => {
+              for (let _id of listCheckedIds) {
+                if (_id === obj._id) {
+                  return false;
+                }
+              }
+              return true;
+            }));
+            Helper.notifyBox('永久删除 ' + json.data.numberOfRemovedDocs.n + ' 个文件成功', 'success');
+          } else {
+            Helper.notifyBox('抱歉, 永久删除失败, 请重试.', 'danger');
+          }
+        }).catch((ex) => {
+          console.log(ex);
+          Helper.notifyBox('抱歉, 永久删除失败, 请重试.', 'danger');
+        })
+    }
+
+    const alertMessage = '确定要永久删除已选的' + listCheckedIds.length + '个文件吗?' + '(请谨慎操作)';
+    ReactDOM.render(<AlertBox alertTitle="确认永久删除" alertMessage={alertMessage} confirm={confirmClick}/>,
+                 document.getElementById('alertBox'));
+  }
+
   render() {
     return (
       <div className="hover-menu-list-wrapper btn-group">
@@ -133,7 +176,7 @@ class HoverMenuList extends Component {
         }
         { this.props.isDelete &&
           <button name="delete" type="button" className="btn btn-primary-outline"
-            >
+            onClick={(e) => this.handleCompleteDeleteButtonClick(e)}>
           <span className="glyphicon glyphicon-trash" aria-hidden="true" style={{position: 'relative', top: '2px'}}></span> 永久删除
           </button>
         }
--- src/components/ToolBarButtonGroup.js
+++ src/components/ToolBarButtonGroup.js
@@ -130,6 +130,45 @@ class ToolBarButtonGroup extends Component {
 
   handleCompleteDeleteButtonClick(event) {
     event.target.blur();
+    const activeLists = this.props.bodyContent.activeLists;
+    const listCheckedIds = this.props.bodyContent.listCheckedIds;
+
+    const confirmClick = () => {
+      const fetchLink = Helper.fetchLinkHeader + 'completeDelete';
+      const params = {
+        ids: listCheckedIds,
+      };
+      fetch(fetchLink, {
+        method: 'POST',
+        body: JSON.stringify(params),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then((response) => response.json())
+        .then((json) => {
+          if (json.success === '1' || json.success === 1) {
+            this.props.onUpdateActiveLists(activeLists.filter((obj) => {
+              for (let _id of listCheckedIds) {
+                if (_id === obj._id) {
+                  return false;
+                }
+              }
+              return true;
+            }));
+            Helper.notifyBox('永久删除 ' + json.data.numberOfRemovedDocs.n + ' 个文件成功', 'success');
+          } else {
+            Helper.notifyBox('抱歉, 永久删除失败, 请重试.', 'danger');
+          }
+        }).catch((ex) => {
+          console.log(ex);
+          Helper.notifyBox('抱歉, 永久删除失败, 请重试.', 'danger');
+        })
+    }
+
+    const alertMessage = '确定要永久删除已选的' + listCheckedIds.length + '个文件吗?' + '(请谨慎操作)';
+    ReactDOM.render(<AlertBox alertTitle="确认永久删除" alertMessage={alertMessage} confirm={confirmClick}/>,
+                 document.getElementById('alertBox'));
   }
 
   render() {
